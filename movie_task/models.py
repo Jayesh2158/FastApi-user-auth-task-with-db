5eb410e86d22527fb9d463fc7a5e1015fe25c939
jayesh2158 <jayeshchoudhary2158@gmail.com>
2021-12-06 18:12:14 +0530

    Update models

--- movie_task/models.py
+++ movie_task/models.py
@@ -1,6 +1,6 @@
 import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import deferred, relationship
+from sqlalchemy.orm import backref, deferred, relationship
 from sqlalchemy.sql.sqltypes import DateTime
 from .database import Base
 
@@ -11,6 +11,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = deferred(Column(String))
+    user_profile_image = Column(String, nullable=True)
     movies = relationship("Movies", back_populates="owner")
 
 
@@ -20,7 +21,8 @@ class Comments(Base):
     id = Column(Integer, primary_key=True, index=True)
     content = Column(String, index=True)
     movie_id = Column(Integer, ForeignKey('movies.id'))
-    movie = relationship("Movies", back_populates="comment")
+    movie = relationship("Movies", backref=backref(
+        "comment", cascade="all, delete-orphan"))
 
 
 class Movies(Base):
@@ -31,9 +33,9 @@ class Movies(Base):
     release_year = Column(Integer, index=True)
     genres = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey('users.id'))
-    owner = relationship("User", back_populates="movies")
+    owner = relationship("User", cascade="save-update")
     comment = relationship(
-        "Comments", back_populates="movie")
+        "Comments", cascade="all, delete-orphan")
 
 
 class Forgotpass_code(Base):
@@ -42,12 +44,11 @@ class Forgotpass_code(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, index=True)
     reset_code = Column(String, index=True)
-    status = Column(String(1), index=True)
     expired_in = Column(DateTime, default=datetime.datetime.utcnow)
 
 
 class Black_list(Base):
     __tablename__ = "black-list"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     token = Column(String, index=True)
